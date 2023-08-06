ee2ea042243ca4520a23c0330c27942ce46c8a8c
Jonatas Silva <jonatas1200@icloud.com>
2023-08-06 10:13:00 -0300

    create route DELETE

--- src/app.js
+++ src/app.js
@@ -42,6 +42,14 @@ app.put('/livros/:id', (req, res) => {
     res.json(livros);
 })
 
+
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    livros.splice(index, 1);
+    res.send(`Livro ${id} removido com sucesso`);
+})
+
 function buscaLivro(id){
     return livros.findIndex(livro => livro.id == id)
 }
