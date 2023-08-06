070b9fca7cb0bba48d05163b6ba3c486fe608354
Jonatas Silva <jonatas1200@icloud.com>
2023-08-06 12:39:39 -0300

    update gitignore v3

--- src/config/dbConnect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://jdeveloperanalyst:jon123@node-express.kcfgmvu.mongodb.net/alura-node");
-
-let db = mongoose.connection;
-
-export default db;
