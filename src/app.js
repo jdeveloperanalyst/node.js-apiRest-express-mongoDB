85a6e7180c7f8479ac3bbd927c32e74ca34e2ff7
Jonatas Silva <jonatas1200@icloud.com>
2023-08-08 21:13:28 -0300

    recriando as rotas integrando com mongoDB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import db from "./config/dbConnect.js"
-import livros from "./models/Livro.js"
 import routes from "./routes/index.js"
 
 //Cria a conexão
@@ -15,45 +14,6 @@ const app = express();
 app.use(express.json());
 
 routes(app);
-
-// const livros = [
-//     {id: 1, "titulo": "Senhor dos Aneis"},
-//     {id: 2, "titulo": "O Hobiit"}
-//  ]
-
-// Requisição de busca por id
-app.get('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    res.json(livros[index]);
-})
-
-//O método de requisição HTTP POST é usado para enviar dados para um servidor
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livro foi cadastrado com sucesso')
-})
-
-/*
-O método de requisição HTTP PUT é usado para enviar dados para um servidor
-com a intenção de atualizar ou criar um recurso específico
-*/
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.json(livros);
-})
-
-
-app.delete('/livros/:id', (req, res) => {
-    let {id} = req.params;
-    let index = buscaLivro(id);
-    livros.splice(index, 1);
-    res.send(`Livro ${id} removido com sucesso`);
-})
-
-function buscaLivro(id){
-    return livros.findIndex(livro => livro.id == id)
-}
 
 //Exporta este arquivo, para que outro arquivo possa fazer uso
 export default app
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,7 +4,23 @@ import LivroController from '../controllers/livrosController.js';
 const router = express.Router();
 
 router
+ //O método de requisição HTTP GET é usado para recuperar informações de um servidor
  .get("/livros", LivroController.listarLivros)
+
+ // Requisição de busca por id
+ .get("/livros/:id", LivroController.listarLivroPorId)
+
+ //O método de requisição HTTP POST é usado para enviar dados para um servidor
+ .post("/livros", LivroController.cadastrarLivro)
+
+ /*
+ O método de requisição HTTP PUT é usado para enviar dados para um servidor
+ com a intenção de atualizar ou criar um recurso específico
+ */
+ .put("/livros/:id", LivroController.atualizarLivro)
+ 
+ //O método de requisição HTTP DELETE é utilizado para solicitar a remoção de um recurso específico no servidor.
+ .delete("/livros/:id", LivroController.excluirLivro)
 
  export default router;
  
